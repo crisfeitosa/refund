a60c67b0b111e3836b07ad2a12644b4d96deb364
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-03-08 18:48:28 -0300

    Removendo um item da lista

--- scripts.js
+++ scripts.js
@@ -155,8 +155,17 @@ function updateTotals() {
 
 // Evento que captura o clique nos itens da lista
 expenseList.addEventListener("click", function (event) {
+
   // Verificar se o elemento clicado é o ícone de remover
   if (event.target.classList.contains("remove-icon")) {
-    console.log(event)
+
+    // Obtém a li pai do elemento clicado
+    const item = event.target.closest(".expense");
+
+    // Remove o item da lista
+    item.remove();
   }
+
+  // Atualiza os totais
+  updateTotals();
 })
